d7bb2dbf2c279fa89fec77d0556547420101062f
Andliage <pox@andliage.com>
2020-05-28 21:51:32 +0800

    环境测试

---
+++ library.sql
@@ -0,0 +1,54 @@
+create table admin
+(
+    id       int AUTO_INCREMENT primary key,
+    username varchar(64),
+    password char(32)
+) ENGINE = InnoDB;
+
+create table user
+(
+    id          int AUTO_INCREMENT primary key,
+    username    varchar(64),
+    password    char(32),
+    max_hold    int,
+    create_time datetime
+) ENGINE = InnoDB;
+
+create table book
+(
+    id     int AUTO_INCREMENT primary key,
+    name   varchar(128),
+    author varchar(64),
+    refer  varchar(16),
+    count  int,
+    status int
+) ENGINE = InnoDB;
+
+create table hold
+(
+    id   int AUTO_INCREMENT primary key,
+    user int,
+    book int,
+    foreign key (user) references user (id),
+    foreign key (book) references book (id)
+) ENGINE = InnoDB;
+
+create table br_log
+(
+    id   int AUTO_INCREMENT primary key,
+    user int,
+    book int,
+    type int,
+    time datetime,
+    foreign key (user) references user (id),
+    foreign key (book) references book (id)
+) ENGINE = InnoDB;
+
+create table admin_operate_log
+(
+    id      int AUTO_INCREMENT primary key,
+    admin   int,
+    time    datetime,
+    content varchar(256),
+    foreign key (admin) references admin (id)
+) ENGINE = InnoDB;
